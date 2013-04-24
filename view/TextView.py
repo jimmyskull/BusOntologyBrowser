de8a0ab4250d90efec4c64ec0a8d8b15214258f3
Geovane <geonnave@palantir.(none)>
2013-04-24 10:24:48 -0300

    update view

--- view/TextView.py
+++ view/TextView.py
@@ -6,11 +6,16 @@ class TextView(View):
 		View.__init__(self)
 	
 	def show_menu(self):
-		print "1. Listar linhas de um ponto"
+		print "1. Listar os horários em que há ônibus para um local."
+		print "2. Listar linhas que passam por um ponto."
+		print "3. Listar os horários de uma linha."
+		print "4. Mostrar um itinerário para um local."
+		print "5. Listar os pontos próximos de um local."
+		print "6. Listar os horários em que há um itinerário de um local inicial para outro local."
 		
 	def read_input(self):
 		MIN = 1
-		MAX = 1
+		MAX = 6
 		try:
 			question = raw_input('Entre um valor ({} - {}) >. '.format(MIN, MAX))	
 			answer = int(question) 
